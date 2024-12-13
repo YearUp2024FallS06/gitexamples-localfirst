92449fb8d6d9e97c2cefa99cbbde1795a9c27889
mattchristenson <matt@rcc.team>
2024-12-13 10:21:37 -0600

    initial commit

---
+++ src/main/java/com/example/gitexamples_localfirst/GitexamplesLocalfirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitexamples_localfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexamplesLocalfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitexamplesLocalfirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitexamples_localfirst/GitexamplesLocalfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitexamples_localfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexamplesLocalfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
